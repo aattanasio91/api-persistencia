770a62ba717d1716fd121b8352497d1e7138644e
Agustin Attanasio <agustin.attanasio@gmail.com>
2022-09-15 21:23:39 -0300

    Agregada entidad materia

---
+++ api/models/materia.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const materia = sequelize.define('materia', {
+    nombre: DataTypes.STRING,
+    id_carrera: DataTypes.INTEGER
+  }, {});
+  materia.associate = function(models) {
+  };
+  return materia;
+};
